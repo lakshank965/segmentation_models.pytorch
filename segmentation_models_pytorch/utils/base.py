39c435603825ab10901a0e93bd6ac42172fb4e84
KavinduLakshanWicramasinghe <lakshank965@gmail.com>
2024-01-29 14:43:03 +0530

    BaseObject change

--- segmentation_models_pytorch/utils/base.py
+++ segmentation_models_pytorch/utils/base.py
@@ -3,18 +3,19 @@ import torch.nn as nn
 
 
 class BaseObject(nn.Module):
-    def __init__(self, name=None):
-        super().__init__()
-        self._name = name
-
-    @property
-    def __name__(self):
-        if self._name is None:
-            name = self.__class__.__name__
-            s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
-            return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
-        else:
-            return self._name
+    # def __init__(self, name=None):
+    #     super().__init__()
+    #     self._name = name
+    #
+    # @property
+    # def __name__(self):
+    #     if self._name is None:
+    #         name = self.__class__.__name__
+    #         s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    #         return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+    #     else:
+    #         return self._name
+    pass
 
 
 class Metric(BaseObject):
